46850666d9be266f9f80cac13fe8e0e44770f982
Alvin Zablan <azablan.dev@gmail.com>
2020-04-22 10:15:22 -0400

    clean up solution

--- fuel-injection-perfection/solution.py
+++ fuel-injection-perfection/solution.py
@@ -13,25 +13,5 @@ def solution(n):
 
     return num_steps
 
-
-# def solution(n):
-#     n = int(n)
-#     steps = 0
-
-#     while n > 1:
-#         # if n is even, divide by 2 using bit manipulation
-#         if n & 1 == 0:
-#             n = n >> 1
-#         else:
-#         # Use bit manipulation to create as many 0 from LSB as possible
-#             if (n == 3) or (n % 4 == 1):
-#                 n = n - 1
-#             else:
-#                 n = n + 1
-#             #   n = (n - 1) if (n == 3 or n % 4 == 1) else (n + 1)
-
-#         steps += 1
-#     return steps
-
 print(solution('15'))   # 5
 print(solution('4'))    # 2
